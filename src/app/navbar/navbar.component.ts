24fc16ed4227013ffafbb7914df902a5da149a6e
Marina Tukalo <marina-tukalo@hotmail.com>
2019-10-27 15:35:44 +0100

    change navbar font

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 	private projectName: string;
   	constructor() { 
-  		this.projectName = "Travelex";
+  		this.projectName = "TRAVELEX";
   	}
 
  	ngOnInit() {
